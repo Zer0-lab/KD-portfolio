508475d6ba7abfb4b9c9c8e5a136eaa9247e98ed
Kévin Dhoust <kd@Ordinateur-portable-de-Kevin.local>
2025-03-03 20:32:19 +0100

    Correction de base dans vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Zer0-lab.github.io/",  // Ajoute cette ligne pour GitHub Pages
+  base: "/",  
 });
