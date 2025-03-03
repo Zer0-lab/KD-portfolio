46aa80aba086b1dc1cecab84108cf4713dfc736a
Kévin Dhoust <kd@Ordinateur-portable-de-Kevin.local>
2025-03-03 20:17:22 +0100

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Zer0-lab.github.io/",  // Ajoute cette ligne pour GitHub Pages
-});
